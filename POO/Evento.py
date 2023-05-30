1fd55d880687606f649672a9d3babe75bd112237
fessouza-dev <joao.fessouza@gmail.com>
2023-05-30 10:33:42 -0300

    Update POO

--- POO/Evento.py
+++ POO/Evento.py
@@ -1,3 +1,5 @@
+import json
+
 class Evento:
     id = 1
     
@@ -12,6 +14,13 @@ class Evento:
         print(f"Nome do evento: {self.nome}")
         print(f"Local do evento: {self.local}")
         print("-----------------------")
+        
+    def to_json(self):
+        return json.dumps({
+            "id": self.id,
+            "local": self.local,
+            "nome": self.nome
+        })
     
     @staticmethod
     def calcula_limite_pessoas_area(area):
--- POO/app.py
+++ POO/app.py
@@ -9,7 +9,9 @@ ev2 = Evento("Aula de JavaScript", "Rio de Janeiro")
 ev2.imprime_informacoes()
 
 ev_online = EventoOnline("Live de Python")
-ev_online.imprime_informacoes()
+# ev_online.imprime_informacoes()
+print(ev_online.to_json())
 
 ev2_online = EventoOnline("Live de JavaScript")
-ev2_online.imprime_informacoes()
+# ev2_online.imprime_informacoes()
+print(ev2_online.to_json())
